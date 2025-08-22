d9a7b5a365bfa4fd15235aed50a96b6f8fd883db
Imtiaze <ahammedimtiaze78@gmail.com>
2025-08-22 22:31:51 +0600

    Count calories function done

--- Javascript/Learn Form Validation By Building A Calorie Counter/script.js
+++ Javascript/Learn Form Validation By Building A Calorie Counter/script.js
@@ -41,9 +41,29 @@ function getCaloriesFromInputs(list) {
 
     if (invalidInputMatch) {
       alert(`Invalid Input: ${invalidInputMatch[0]}`);
-
+      isError = true;
+      return null;
     }
+    return calories += Number(currVal);
   }
+
+  return calories;
 }
+
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type="number"]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type="number"]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type="number"]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type="number"]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type="number"]');
+  
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+} 
 
 addEntryButton.addEventListener("click", addEntry);
