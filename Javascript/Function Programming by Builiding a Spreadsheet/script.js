2b067956b5708b0f629944a528b1072dd6365854
Imtiaze <ahammedimtiaze78@gmail.com>
2025-11-20 17:01:16 +0600

    Regex added for matching column and row like A12:B9

--- Javascript/Function Programming by Builiding a Spreadsheet/script.js
+++ Javascript/Function Programming by Builiding a Spreadsheet/script.js
@@ -11,10 +11,19 @@ const median = nums => {
     : sorted[Math.ceil(middle)];
 }
 
-
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median
+}
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
+
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+}
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -33,7 +42,16 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
       container.appendChild(input);
     })
   })
+}
+
+const update = event => {
+  const element = event.target;
+  const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id) && value.startsWith('=')) {
+
+  }
 }
